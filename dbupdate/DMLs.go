172c2a8d7ec2023abed4d9a438a64092d4a14ad1
Surush <davlatzoda.surush@gmail.com>
2020-02-09 13:30:09 +0500

    change registor

--- dbupdate/DMLs.go
+++ dbupdate/DMLs.go
@@ -1,7 +1,7 @@
 package dbupdate
 
-const addClientDML = `insert into clients(name, surname, login, password, locked) 
-values ($1, $2, $3, $4, $5)`
+const addClientDML = `insert into clients(name, surname, login, password, phone, locked) 
+values ($1, $2, $3, $4, $5, $6)`
 
 const addATM = `insert into ATMs(name, locked)
 values ($1, $2)`
